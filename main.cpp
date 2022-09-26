fb924793ef5e5ff980281f891734eb88228bdfaa
Oskar Lundmark <oskar.lundmark@live.se>
2022-09-26 12:28:43 +0200

    add price-check

--- main.cpp
+++ main.cpp
@@ -1,56 +1,91 @@
 #include <iostream>
 using namespace std;
 
-
 void printWelcome() {
-    cout << "Välkommen, detta program räknar ut antal sedlar och mynt som en kund får tillbaka vid köp. " << endl;
+    //print welcome message.
+    cout << "Välkommen, detta program räknar ut antal\nsedlar och mynt som en kund får tillbaka vid köp. " << endl;
     cout << "Vänligen följ angivna instruktioner" << endl;
 }
 
-int getInteger() {
-    int input = 0;
+long getInteger() {
+    //Get only integers, prints error message if not int.
+    long input = 0;
     while(true) {
         if (cin >> input) {
             return input;
         }
         else {
-            cout << "Fel typ av data, försök igen.\n";
+            cout << "Programmet tar bara emot heltal, försök igen.\n";
             cin.clear();
             cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
             input = 0;
         }
     }
-    }
+}
 
-int getTotal(int balance){
-    int numProducts;
+bool checkPrice( long price){
+        cout << "OBS! Kontrollera pris." << endl;
+        cout << "Angivet pris: " << price << endl;
+        cout << "Vill du fortsätta) (y/n)" << endl;
+        string ans;
+        while (true) {
+            if (cin >> ans) {
+                if (ans == "y") {
+                    return true;
+                }
+                if (ans == "n") {
+                    return false;
+                } else {
+                    cout << "Var god svara med y för ja eller n för nej.\n";
+                }
+            } else {
+                cout << "Var god svara med y för ja eller n för nej\n";
+                cin.clear();
+                cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+            }
+        }
+}
+
+long getTotal(long balance){
+    // Get number of vares and updates balance.
+    long numProducts;
     cout << "Ange antal varor: ";
     numProducts = getInteger();
     for (int i = 1; i < numProducts + 1; i++) {
+        long price = 0;
         cout << "Kostnad vara " << i << ": ";
-        balance -= getInteger();
+        price = getInteger();
+        if (price > 0) {
+            balance -= price;
+        }
+        if (price < 0) {
+            if (checkPrice(price)){
+            balance -= price;
+            } else {
+            i--;
+            } 
+        }
+        cout << "Att betala: " << balance * -1 << endl;
     }
-    cout << "Total kostnad:" << balance * -1 << "kr" << endl;
     return balance;
 }
 
-
-int getMore(int balance) {
+long getMore(long balance) {
+    // Ask for more money.
     while (balance< 0) {
-            cout << "Kvar att betala: " << balance * -1 << "sek"<< endl;
+            cout << "Att betala: " << balance * -1 << "sek"<< endl;
             cout << "Ange mottaget belopp:";
             balance += getInteger();
     }
     return balance;
 }
 
-void printChange( int balance) {
+void printChange( long balance) {
         int denominations[] = {1000, 500, 100, 50, 20, 10, 5, 1};
         int denom_back;
-        cout << "Totalt tillbaka: " << balance << "kr\n";
+        cout << "Åter: " << balance << "kr\n";
         cout << "Valörer: \n";
         for (int denomination: denominations) {
-            // Deconstructed modulus.
             denom_back = balance / denomination;
             balance = balance % denomination;
             // Only print what costumer needs back.
@@ -63,7 +98,7 @@ void printChange( int balance) {
 bool askRestart() {
     //Asks if user wants to make another purchase.
     string input;
-    cout << "Vill du göra ett till köp? (y/n)" << endl;
+    cout << "Vill du göra ett nytt köp? (y/n)" << endl;
     while (true) {
         if (cin >> input) {
             if (input == "y") {
@@ -83,12 +118,12 @@ bool askRestart() {
 }
 
 int main() {
-    int balance = 0;
     bool on = true;
 
     printWelcome();
 
     while (on) {
+        long balance = 0;
         balance = getTotal(balance);
         cout << "Ange mottaget belopp:";
         balance += getInteger();
@@ -107,6 +142,5 @@ int main() {
         }
         on = askRestart();
     }
-
     return 0;
 }
